58c421977f638d9f099e6e91d497aa005c1599a3
Mirzhana <mrarmkrrr@gmail.com>
2018-11-23 09:42:53 +0600

    Initial commit

---
+++ app/src/main/java/com/ayanamirzhana/mypet/AddAnnounceFragment.kt
@@ -0,0 +1,17 @@
+package com.ayanamirzhana.mypet
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AddAnnounceFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.fragment_add_announce, container, false)
+
+    companion object {
+        fun newInstance(): AddAnnounceFragment = AddAnnounceFragment()
+    }
+}
---
+++ app/src/main/java/com/ayanamirzhana/mypet/HomeActivity.kt
@@ -0,0 +1,44 @@
+package com.ayanamirzhana.mypet
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                val homeFragment = HomeFragment.newInstance()
+                openFragment(homeFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_add -> {
+                val addFragment = AddAnnounceFragment.newInstance()
+                openFragment(addFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_profile -> {
+                val profileFragment = ProfileFragment.newInstance()
+                openFragment(profileFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
